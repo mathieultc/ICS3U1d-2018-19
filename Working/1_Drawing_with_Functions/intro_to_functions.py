64d549171ff50dd2db3cb06e8cc153fa04b3ca5f
robuntu <robuntu@stro.ycdsb.ca>
2019-03-05 11:10:23 -0500

    drawing functions

--- Working/1_Drawing_with_Functions/intro_to_functions.py
+++ Working/1_Drawing_with_Functions/intro_to_functions.py
@@ -4,6 +4,7 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
 # define your draw functions
+arcade.start_render()
 
 def draw_cloud():
     '''Draw white clouds '''
@@ -20,20 +21,38 @@ def draw_hill():
     arcade.draw_circle_filled(150,-190,400,arcade.color.DARK_GREEN)
     arcade.draw_circle_filled(600,-150,300,arcade.color.DARK_GREEN)
 
+def draw_tree():
+      arcade.draw_circle_filled(250,300, 50, arcade.color.DARK_GREEN)
+      arcade.draw_circle_filled(290,270, 30, arcade.color.DARK_GREEN)
+      arcade.draw_circle_filled(260,260, 30, arcade.color.DARK_GREEN)
+      arcade.draw_circle_filled(230,250,40,arcade.color.DARK_GREEN)
+      arcade.draw_rectangle_filled(250, 230, 45, 150,arcade.color.PALE_BROWN)
+
+def draw_sun():
+    arcade.draw_circle_filled(50,600,70, arcade.color.YELLOW)
 
 def main():
     arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, "Drawing with Functions")
-    arcade.set_background_color(arcade.color.SKY_BLUE)
+    arcade.set_background_color(arcade.color.SKY_BLUE
+                                )
     arcade.start_render()
 
    # call your draw functions
     draw_cloud()
+    draw_tree()
     draw_hill()
+    draw_sun()
 
-    # Finish and run
     arcade.finish_render()
     arcade.run()
 
-
-# Call the main function to get the program started.
 main()
+
+
+
+
+
+
+
+
+
---
+++ Working/functions_practice.py
@@ -0,0 +1,11 @@
+def happy():
+    print("happy  birthday to you")
+def sing(person):
+    happy()
+    happy()
+    print("Happy birthday to you "  + person)
+
+sing("FRED")
+print("")
+sing("LUCY")
+print("\n")
