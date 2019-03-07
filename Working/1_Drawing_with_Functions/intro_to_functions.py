dd37ac817793e43093423dc9432253b390eac623
robuntu <robuntu@stro.ycdsb.ca>
2019-03-07 10:49:05 -0500

    drawing functions

--- Working/1_Drawing_with_Functions/intro_to_functions.py
+++ Working/1_Drawing_with_Functions/intro_to_functions.py
@@ -1,14 +1,13 @@
 import arcade
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 900
+SCREEN_HEIGHT = 800
 
 # define your draw functions
 arcade.start_render()
 
+#Parameters for clouds
 def draw_cloud(x,y):
-    '''Draw white clouds '''
-
     arcade.draw_circle_filled(250 + x, 500 + y, 70, arcade.color.WHITE)
     arcade.draw_circle_filled(330 + x, 550 + y, 50, arcade.color.WHITE)
     arcade.draw_circle_filled(390 + x, 500 + y, 70, arcade.color.WHITE)
@@ -18,10 +17,12 @@ def draw_cloud(x,y):
     arcade.draw_circle_filled(790 + x, 500 + y, 70, arcade.color.WHITE)
     arcade.draw_circle_filled(730 + x, 470 + y, 40, arcade.color.WHITE)
 
+#Parameters for hill
 def draw_hill():
     arcade.draw_circle_filled(150,-190,400,arcade.color.DARK_GREEN)
     arcade.draw_circle_filled(600,-150,300,arcade.color.DARK_GREEN)
 
+#Parameters for tree
 def draw_tree(x,y):
       arcade.draw_circle_filled(250+x,300+y, 50, arcade.color.DARK_GREEN)
       arcade.draw_circle_filled(290+x,270+y, 30, arcade.color.DARK_GREEN)
@@ -29,59 +30,62 @@ def draw_tree(x,y):
       arcade.draw_circle_filled(230+x,250+y,40,arcade.color.DARK_GREEN)
       arcade.draw_rectangle_filled(250+x, 230+y, 45, 150,arcade.color.PALE_BROWN)
 
-def draw_sun():
-    arcade.draw_circle_filled(50,600,70, arcade.color.YELLOW)
+#parameters for apple
+def draw_apple(x,y):
+    arcade.draw_circle_filled(x,y,10,arcade.color.RED)
 
+def draw_sun(x,y):
+    arcade.draw_circle_filled(x,600+y,70, arcade.color.YELLOW)
+
+    arcade.finish_render()
+
+#moving sun
 def on_draw(delta_time):
-    global sun
+    global draw_sun_x
 
     """ Draw everything """
     arcade.start_render()
 
-    draw_sun(sun, 140)
-    draw_sun(450, 180)
+    draw_tree(10,20)
+    draw_tree(70,-10)
+    draw_tree(300,-10)
+    draw_apple(250,350)
+    draw_apple(220,300)
+    draw_apple(280,250)
+    draw_apple(280,300)
+    draw_hill()
+
+    draw_sun(draw_sun_x, 140)
+    draw_cloud(draw_cloud_x,140)
 
     # Add one to the x value, making the snow person move right
     # Negative numbers move left. Larger numbers move faster.
-    sun += 1
+    draw_sun_x += 1
 
+draw_sun_x = 140
 
-# Create a value that our snow_person1_x will start at.
-sun = 150
+#moving cloud
+def moving(delta_time):
+    global draw_cloud_x
+
+    arcade.start_render()
+
+    draw_cloud(draw_cloud_x,100)
+    draw_cloud_x += 1
+
+#Create a value that our snow_person1_x will start at.
+draw_cloud_x = 100
 
 def main():
     arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, "Drawing with Functions")
-    arcade.set_background_color(arcade.color.SKY_BLUE
-                                )
-    arcade.start_render()
+    arcade.set_background_color(arcade.color.SKY_BLUE)
 
-   # call your draw functions
-    draw_cloud(100,10)
-    draw_cloud(200,40)
-    draw_cloud(250,80)
-    draw_tree(10,20)
-    draw_tree(50,50)
-    draw_tree(100,300)
-    draw_hill()
-    draw_sun()
-
-        # Call on_draw every 60th of a second.
-   arcade.schedule(on_draw, 1 / 60)
-   arcade.run()
-
-    # Call the main function to get the program started.
-
-
+    # Call on_draw every 60th of a second.
+    arcade.schedule(on_draw, 1/30)
+    arcade.schedule(moving, 1/60)
     arcade.finish_render()
     arcade.run()
 
+
+# Call the main function to get the program started.
 main()
-
-
-
-
-
-
-
-
-
