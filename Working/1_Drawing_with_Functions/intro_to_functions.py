5e6e2b40ece573838eff5293fc32de85a1d9b8bc
mathieu <mathieu.litimcheong20@ycdsbk12.ca>
2019-03-18 20:59:47 -0400

    volumes and drawings

--- Working/1_Drawing_with_Functions/intro_to_functions.py
+++ Working/1_Drawing_with_Functions/intro_to_functions.py
@@ -37,26 +37,43 @@ def draw_apple(x,y):
 def draw_sun(x,y):
     arcade.draw_circle_filled(x,600+y,70, arcade.color.YELLOW)
 
-    arcade.finish_render()
+def moving(delta_time):
+    global draw_cloud_x
+    arcade.start_render()
 
-#moving sun
+    draw_tree(10, 20)
+    draw_tree(70, -10)
+    draw_tree(300, -10)
+    draw_apple(250, 350)
+    draw_apple(220, 300)
+    draw_apple(280, 250)
+    draw_apple(280, 300)
+    draw_hill()
+
+    draw_cloud(draw_cloud_x,100)
+
+    draw_cloud_x += 1
+
+#Create a value that our snow_person1_x will start at.
+draw_cloud_x = 100
+
 def on_draw(delta_time):
     global draw_sun_x
 
     """ Draw everything """
     arcade.start_render()
 
-    draw_tree(10,20)
-    draw_tree(70,-10)
-    draw_tree(300,-10)
-    draw_apple(250,350)
-    draw_apple(220,300)
-    draw_apple(280,250)
-    draw_apple(280,300)
+    draw_tree(10, 20)
+    draw_tree(70, -10)
+    draw_tree(300, -10)
+    draw_apple(250, 350)
+    draw_apple(220, 300)
+    draw_apple(280, 250)
+    draw_apple(280, 300)
     draw_hill()
 
+
     draw_sun(draw_sun_x, 140)
-    draw_cloud(draw_cloud_x,140)
 
     # Add one to the x value, making the snow person move right
     # Negative numbers move left. Larger numbers move faster.
@@ -64,28 +81,15 @@ def on_draw(delta_time):
 
 draw_sun_x = 140
 
-#moving cloud
-def moving(delta_time):
-    global draw_cloud_x
-
-    arcade.start_render()
-
-    draw_cloud(draw_cloud_x,100)
-    draw_cloud_x += 1
-
-#Create a value that our snow_person1_x will start at.
-draw_cloud_x = 100
 
 def main():
     arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, "Drawing with Functions")
     arcade.set_background_color(arcade.color.SKY_BLUE)
 
     # Call on_draw every 60th of a second.
-    arcade.schedule(on_draw, 1/30)
-    arcade.schedule(moving, 1/60)
-    arcade.finish_render()
+    arcade.schedule(moving,1/60)
+    arcade.schedule(on_draw,1/30)
     arcade.run()
-
 
 # Call the main function to get the program started.
 main()
