2967e66badc7c3d0e7bf063e140c036777ffa807
robuntu <robuntu@stro.ycdsb.ca>
2019-05-14 11:09:51 -0400

    Merge branch 'master' of https://github.com/mathieultc/ICS3U1d-2018-19
    # Conflicts:
    #	Working/1_Drawing_with_Functions/intro_to_functions.py

--- Masters/2_Control_Flow/four loops/loops.py
+++ Masters/2_Control_Flow/four loops/loops.py
@@ -1,6 +1,16 @@
-def km_to_miles():
+print("{0:>10} {01:>10}".format("Price","You Pay"))
+print("{0:25}".format("--------------------------"))
 
-    for i in range(5,51,5):
-        print(i, "km =" , round((i*0.62),2), "miles")
+#compute the discount and output the table
+for price in range(10,151,10):
+    if price < 20:
+        cost = price-(price*0.1)
+    elif price >= 20 and price < 50:
+        cost = price-(price*0.2)
+    elif price >= 50 and price < 100:
+        cost = price-(price*0.3)
+    else:
+        cost = price-(price*0.4)
 
-km_to_miles()
+    print("{0:10.2f} {1:10.2f}".format(price,cost))
+
---
+++ Working/player_movement.py
@@ -0,0 +1,79 @@
+import arcade
+
+
+WIDTH = 640
+HEIGHT = 480
+
+# start player position in middle of window
+player_x = WIDTH/2
+player_y = HEIGHT/2
+
+# Variables to record if certain keys are being pressed.
+up_pressed = False
+down_pressed = False
+left_pressed = False
+right_pressed = False
+
+
+def on_update(delta_time):
+    global up_pressed, player_y, player_x,down_pressed,right_pressed,left_pressed
+    if up_pressed:
+        player_y += 100
+    if down_pressed:
+        player_y -= 100
+    if right_pressed:
+        player_x += 100
+    if left_pressed:
+        player_x -= 1000
+
+
+def on_draw():
+    global player_x, player_y
+    arcade.start_render()
+    # Draw in here...
+    arcade.draw_circle_filled(player_x, player_y, 25, arcade.color.BLUE)
+
+
+def on_key_press(key, modifiers):
+    global up_pressed,down_pressed,right_pressed,left_pressed
+    if key == arcade.key.W:
+        up_pressed = True
+    if key == arcade.key.S:
+        down_pressed = True
+    if key == arcade.key.D:
+        right_pressed = True
+    if key == arcade.key.A:
+        left_pressed = True
+
+def on_key_release(key, modifiers):
+    global up_pressed,down_pressed,right_pressed,left_pressed
+    if key == arcade.key.W:
+        up_pressed = False
+    if key == arcade.key.S:
+        down_pressed = False
+    if key == arcade.key.D:
+        right_pressed = False
+    if key == arcade.key.A:
+        left_pressed = False
+
+
+def on_mouse_press(x, y, button, modifiers):
+    pass
+
+
+def setup():
+    arcade.open_window(WIDTH, HEIGHT, "My Arcade Game")
+    arcade.set_background_color(arcade.color.WHITE)
+    arcade.schedule(on_update, 1/60)
+
+    # Override arcade window methods
+    window = arcade.get_window()
+    window.on_draw = on_draw
+    window.on_key_press = on_key_press
+    window.on_key_release = on_key_release
+
+    arcade.run()
+
+
+if __name__ == '__main__':
+    setup()
